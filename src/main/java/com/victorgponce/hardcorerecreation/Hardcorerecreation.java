ef49520252b54cd6b492650558755c58b8bffe36
ponchisao326 <ponchisao326@hotmail.com>
2026-09-06 14:07:43 +0200

    Una poca de spam

--- src/main/java/com/victorgponce/hardcorerecreation/Hardcorerecreation.java
+++ src/main/java/com/victorgponce/hardcorerecreation/Hardcorerecreation.java
@@ -1,11 +1,16 @@
 package com.victorgponce.hardcorerecreation;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Hardcorerecreation implements ModInitializer {
+    public static final String MODID = "hardcorerecreation";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Made with ❤️ by Ponchisao326 (Victor G. Ponce)");
         MobNaming.register();
         ResetTrigger.register();
     }
