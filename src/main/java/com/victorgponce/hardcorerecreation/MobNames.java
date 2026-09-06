f8686e1f8bb9b5e17a77f879af8391cf437ffbde
ponchisao326 <ponchisao326@hotmail.com>
2026-09-06 14:04:23 +0200

    update Enderman name

--- src/main/java/com/victorgponce/hardcorerecreation/MobNames.java
+++ src/main/java/com/victorgponce/hardcorerecreation/MobNames.java
@@ -2,7 +2,7 @@ package com.victorgponce.hardcorerecreation;
 
 public final class MobNames {
 
-    public static final String ENDERMAN = "Endermano";
+    public static final String ENDERMAN = "Ni germen";;
 
     public static final String CHICKEN = "Ni gallo ni pollo";
 
